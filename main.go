f7415beaa83b83d40b2cffa4eeab27d94272d888
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2025-02-09 17:04:54 +0300

    add catch and inspect commands

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -38,10 +39,37 @@ type specificLocationAreaResponse struct {
 	} `json:"pokemon_encounters"`
 }
 
+type Pokemon struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	Types          []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+	Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+}
+
 type cliCommand struct {
 	name        string
 	description string
 	callback    func(c *config, name string) error
+}
+
+type User struct {
+	Pokedex map[string]Pokemon
 }
 
 func cleanInput(text string) []string {
@@ -88,12 +116,41 @@ func (c *config) printPokemonNames(data []byte) error {
 	return nil
 }
 
+func (c *config) printPokemonIsCatched(data []byte, name string, u *User) error {
+	pokemonStruct := Pokemon{}
+
+	err := json.Unmarshal(data, &pokemonStruct)
+	if err != nil {
+		return err
+	}
+
+	K := 100.0
+
+	successProb := 1 / (1 + float64(pokemonStruct.BaseExperience)/K)
+	randomValue := rand.Float64()
+
+	if randomValue < successProb {
+		fmt.Println()
+		fmt.Printf("%s was caught!", name)
+		u.Pokedex[name] = pokemonStruct
+		fmt.Println()
+	} else {
+		fmt.Println()
+		fmt.Printf("%s escaped!", name)
+		fmt.Println()
+	}
+
+	return nil
+}
+
 var pokeCache *pokecache.Cache
+var user User
 
 func main() {
 	pokeCache = pokecache.NewCache(5 * time.Minute)
 	scanner := bufio.NewScanner(os.Stdin)
 	commands := map[string]cliCommand{}
+	user.Pokedex = map[string]Pokemon{}
 
 	commandExit := func(c *config, name string) error {
 		fmt.Println("")
@@ -180,6 +237,67 @@ func main() {
 		return nil
 	}
 
+	commandCatch := func(c *config, name string) error {
+		fmt.Println()
+		fmt.Printf("Throwing a Pokeball at %s...\n", name)
+
+		url := "https://pokeapi.co/api/v2/pokemon/" + name
+
+		if cachedData, ok := pokeCache.Get(url); ok {
+			c.printPokemonIsCatched(cachedData, name, &user)
+			return nil
+		}
+
+		res, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		if res.StatusCode > 299 {
+			return fmt.Errorf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		}
+
+		pokeCache.Add(url, body)
+
+		c.printPokemonIsCatched(body, name, &user)
+
+		return nil
+	}
+
+	commandInspect := func(c *config, name string) error {
+		userPokedex := user.Pokedex
+		pokemon, exists := userPokedex[name]
+
+		if !exists {
+			fmt.Println()
+			fmt.Println("you have not caught that pokemon")
+			fmt.Println()
+			return nil
+		}
+
+		fmt.Printf("Name: %s\n", pokemon.Name)
+		fmt.Printf("Height: %d\n", pokemon.Height)
+		fmt.Printf("Weight: %d\n", pokemon.Weight)
+
+		fmt.Println("Stats:")
+		for _, s := range pokemon.Stats {
+			fmt.Printf("  -%s: %d\n", s.Stat.Name, s.BaseStat)
+		}
+
+		fmt.Println("Types:")
+		for _, t := range pokemon.Types {
+			fmt.Printf("  - %s\n", t.Type.Name)
+		}
+
+		return nil
+	}
+
 	commands["exit"] = cliCommand{
 		name:        "exit",
 		description: "Exit the Pokedex",
@@ -202,6 +320,18 @@ func main() {
 		name:        "explore",
 		description: "Displays a list of all the Pokemon located there",
 		callback:    commandExplore,
+	}
+
+	commands["catch"] = cliCommand{
+		name:        "catch",
+		description: "Catch a pokemon",
+		callback:    commandCatch,
+	}
+
+	commands["inspect"] = cliCommand{
+		name:        "inspect",
+		description: "Inspect one of your pokemons details",
+		callback:    commandInspect,
 	}
 
 	c := config{}
